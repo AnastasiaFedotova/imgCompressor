b1957bf033fbbbc32a167b205cb2dcd8ea42c60c
Anastasia <anastaisafedotova@gmail.com>
2021-07-12 22:15:31 +0200

    feat: add form

--- src/app/components/upload-form/upload-form.component.ts
+++ src/app/components/upload-form/upload-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-upload-form',
   templateUrl: './upload-form.component.html',
   styleUrls: ['./upload-form.component.scss']
 })
 export class UploadFormComponent implements OnInit {
-
+  uploadForm = new FormGroup({
+    image: new FormControl('', Validators.required)
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+
+  }
 }
